c0927edeeef358f1b8d1b3afad43776d6e8ec51a
Felipe Delfim <felipe@delfim.net>
2017-03-30 22:22:34 -0300

    CEDE ERP first commit

--- cede_erp/urls.py
+++ cede_erp/urls.py
@@ -1,4 +1,4 @@
-"""cede_crm URL Configuration
+"""cede_erp URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
--- cede_erp/wsgi.py
+++ cede_erp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for cede_crm project.
+WSGI config for cede_erp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cede_crm.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cede_erp.settings")
 
 application = get_wsgi_application()
