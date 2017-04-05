63df3a57b5403e0135f1b5ab61e8151778acc4cd
Felipe Delfim <felipe@delfim.net>
2017-04-05 18:23:02 -0300

    Changing form labels to portuguese

--- cashflow/forms.py
+++ cashflow/forms.py
@@ -1,13 +1,21 @@
 from django import forms
 from .models import Transaction
+from django.utils.translation import ugettext_lazy as _
 
 class TransactionForm(forms.ModelForm):
 
     class Meta:
         model = Transaction
-        fields = ('item',
-            'person',
-            'method',
-            'amount',
-            'comments',
-            'paid_at',)
+        exclude = ['created_at', 'updated_at']
+        #localized_fields = ['__all__']
+        labels = {
+            'item': _('Item'),
+            'person': _('Frequentador'),
+            'method': _('Forma de Pagamento'),
+            'amount': _('Quantidade'),
+            'comments': _('Comentários'),
+            'paid_at': _('Pago em'),
+        }
+        widgets = {
+            'paid_at': forms.DateInput(attrs={'class':'datepicker'}),
+        }
